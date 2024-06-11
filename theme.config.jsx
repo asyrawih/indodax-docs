ba816b82e42b70f27d6f4c48ba82d904d8167615
asyrawih <hasyrawi@gmail.com>
2024-06-12 03:08:22 +0700

    add meta tag

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,4 +1,13 @@
 export default {
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta data-n-head="ssr" data-hid="og:image" name="og:image" property="og:image" content="https://indodax.com/v3-exchange/card.png" />
+      <meta data-n-head="ssr" data-hid="og:site_name" name="og:site_name" property="og:site_name" content="INDODAX" />
+      <meta data-n-head="ssr" data-hid="og:url" name="og:url" property="og:url" content="https://indodax.com" />
+      <meta data-n-head="ssr" data-hid="og:type" name="og:type" property="og:type" content="website" />
+    </>
+  ),
   logo: <span>Indodax API Docs</span>,
   project: {
     link: 'https://github.com/btcid/indodax-official-api-docs'
