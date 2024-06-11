3ba1ab8080f925665718d413efe988edcbd4ba57
asyrawih <hasyrawi@gmail.com>
2024-06-12 02:48:49 +0700

    add module

---
+++ theme.config.jsx
@@ -0,0 +1,26 @@
+export default {
+  logo: <span>Indodax API Docs</span>,
+  project: {
+    link: 'https://github.com/btcid/indodax-official-api-docs'
+  },
+  docsRepositoryBase: "https://github.com/btcid/indodax-official-api-docs",
+  footer: {
+    text: (
+    <span>
+        MIT {new Date().getFullYear()} ©{' '}
+        <a href="https://nextra.site" target="_blank">
+          Indodax
+        </a>
+        .
+      </span>
+    ) 
+  },
+  navigation: {
+    prev: true,
+    next: true
+  },
+  feedback: {
+    content: 'Need Help?',
+    labels: 'Need help?'
+  }
+}
